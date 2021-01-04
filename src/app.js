3f6b0e14d64490fa526cfe21abaa0521f0b16a6a
topher-dunlap <thelonioustopher@yahoo.com>
2021-01-03 20:55:24 -0600

    first commit

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const {NODE_ENV} = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express()
 
@@ -15,19 +16,50 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
+        .then(articles => {
+            res.json(articles)
+        })
+        .catch(next)
+})
+
 app.get('/', (req, res) => {
     res.send("Hello, world!")
 })
 
- app.use(function errorHandler(error, req, res, next) {
-     let response
-     if (NODE_ENV === 'production') {
-         response = { error: { message: 'server error' } }
-     } else {
-         console.error(error)
-         response = { message: error.message, error }
-     }
-     res.status(500).json(response)
- })
+app.use(function errorHandler(error, req, res, next) {
+    let response
+    if (NODE_ENV === 'production') {
+        response = {error: {message: 'server error'}}
+    } else {
+        console.error(error)
+        response = {message: error.message, error}
+    }
+    res.status(500).json(response)
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+    ArticlesService.getById(
+        req.app.get('db'),
+        req.params.article_id
+    )
+        .then(articles => {
+            if (!article) {
+                return res.status(404).json({
+                    error: { message: `Article doesn't exist` }
+                })
+            }
+            res.json({
+                id: article.id,
+                title: article.title,
+                style: article.style,
+                content: article.content,
+                date_published: new Date(article.date_published),
+            })
+        })
+        .catch(next)
+})
 
 module.exports = app
